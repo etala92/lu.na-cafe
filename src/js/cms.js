d999fcecc717b13c6af53cda5c0e1ba892e9b60a
etala92 <110794310+etala92@users.noreply.github.com>
2022-11-28 17:09:24 +0100

    Changed theme to hugo-theme-ava

--- src/js/cms.js
+++ src/js/cms.js
@@ -2,7 +2,7 @@ import React from "react";
 import CMS from "netlify-cms-app";
 
 // Import main site styles as a string to inject into the CMS preview pane
-import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.scss";
+// import styles from "!to-string-loader!css-loader!postcss-loader!sass-loader!../css/main.scss";
 
 import HomePreview from "./cms-preview-templates/home";
 import PostPreview from "./cms-preview-templates/post";
@@ -10,7 +10,7 @@ import ProductsPreview from "./cms-preview-templates/products";
 import ValuesPreview from "./cms-preview-templates/values";
 import ContactPreview from "./cms-preview-templates/contact";
 
-CMS.registerPreviewStyle(styles, { raw: true });
+// CMS.registerPreviewStyle(styles, { raw: true });
 CMS.registerPreviewTemplate("home", HomePreview);
 CMS.registerPreviewTemplate("post", PostPreview);
 CMS.registerPreviewTemplate("products", ProductsPreview);
